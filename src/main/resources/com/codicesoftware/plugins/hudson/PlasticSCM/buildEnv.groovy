f2ebedf9196823d8aad6e7fb9ae57c42b276cf48
Míryam <mgomez@codicesoftware.com>
2018-08-07 09:27:58 +0000

    Include the plastic env vars to the available ones, which can be viewed through the following url: https://<your-jenkins>/env-vars.html

---
+++ src/main/resources/com/codicesoftware/plugins/hudson/PlasticSCM/buildEnv.groovy
@@ -0,0 +1,9 @@
+package com.codicesoftware.plugins.hudson;
+
+def l = namespace(lib.JenkinsTagLib)
+
+["PLASTICSCM_CHANGESET_ID","PLASTICSCM_CHANGESET_GUID","PLASTICSCM_BRANCH","PLASTICSCM_AUTHOR","PLASTICSCM_REPSPEC"].each { name ->
+    l.buildEnvVar(name:name) {
+        raw(_("${name}.blurb"))
+    }
+}

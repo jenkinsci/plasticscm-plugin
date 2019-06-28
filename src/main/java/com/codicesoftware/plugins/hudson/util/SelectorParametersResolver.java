afc79181585991780261220a7a39356292b38d02
Krzysztof Knapik <knapsu@gmail.com>
2019-06-28 21:21:41 +0200

    Add resolver tests

--- src/main/java/com/codicesoftware/plugins/hudson/util/SelectorParametersResolver.java
+++ src/main/java/com/codicesoftware/plugins/hudson/util/SelectorParametersResolver.java
@@ -6,24 +6,21 @@ import hudson.model.ParameterValue;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 public class SelectorParametersResolver {
 
-    private static final Logger logger = Logger.getLogger(SelectorParametersResolver.class.getName());
-
-    public static String resolve(
-            String selector, List<ParameterValue> parameters) {
-        if (parameters == null)
-            return selector;
-
-        logger.info("Replacing build parameters in selector...");
+    public static String resolve(String text, List<ParameterValue> parameters) {
+        if (parameters == null) {
+            return text;
+        }
 
         Map<String, String> parametersMap = new HashMap<>();
         for (ParameterValue parameter : parameters) {
-            parametersMap.put(parameter.getName(), parameter.getValue().toString());
+            if ((parameter != null) && (parameter.getName() != null) && (parameter.getValue() != null)) {
+                parametersMap.put(parameter.getName(), parameter.getValue().toString());
+            }
         }
 
-        return Util.replaceMacro(selector, parametersMap);
+        return Util.replaceMacro(text, parametersMap);
     }
 }
---
+++ src/test/java/com/codicesoftware/plugins/hudson/util/SelectorParametersResolverTest.java
@@ -0,0 +1,27 @@
+package com.codicesoftware.plugins.hudson.util;
+
+import hudson.model.BooleanParameterValue;
+import hudson.model.ParameterValue;
+import hudson.model.StringParameterValue;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SelectorParametersResolverTest {
+
+    @Test
+    public void testResolver() {
+        List<ParameterValue> parameters = new LinkedList<>();
+        parameters.add(new StringParameterValue("PARAM", "VALUE"));
+        parameters.add(new BooleanParameterValue("BOOL", true));
+
+        assertEquals("sample VALUE text", SelectorParametersResolver.resolve("sample $PARAM text", parameters));
+        assertEquals("sample VALUE text", SelectorParametersResolver.resolve("sample ${PARAM} text", parameters));
+        assertEquals("sample PARAM text", SelectorParametersResolver.resolve("sample PARAM text", parameters));
+        assertEquals("sample true text", SelectorParametersResolver.resolve("sample $BOOL text", parameters));
+    }
+
+}

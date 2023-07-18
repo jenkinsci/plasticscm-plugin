81d931c2fe13004a930a11795f143e4a4b1cefe5
Miguel González <miguel.gonzalez@unity3d.com>
2023-07-18 11:38:36 +0200

    Refactor `ChangeSetID` into `ObjectSpec`

---
+++ src/main/java/com/codicesoftware/plugins/hudson/model/ObjectSpec.java
@@ -0,0 +1,37 @@
+package com.codicesoftware.plugins.hudson.model;
+
+import com.codicesoftware.plugins.jenkins.mergebot.ObjectSpecType;
+
+public class ObjectSpec {
+    private final ObjectSpecType specObjectType;
+    private final int id;
+    private final String repository;
+    private final String server;
+
+    public ObjectSpec(ObjectSpecType specObjectType, int id, String repository, String server) {
+        this.specObjectType = specObjectType;
+        this.id = id;
+        this.repository = repository;
+        this.server = server;
+    }
+
+    public ObjectSpecType getType() {
+        return specObjectType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRepository() {
+        return repository;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public String getFullSpec() {
+        return String.format("%s:%d@%s@%s", specObjectType.toSpecObject(), id, repository, server);
+    }
+}
---
+++ src/test/java/com/codicesoftware/plugins/hudson/model/ObjectSpecTest.java
@@ -0,0 +1,39 @@
+package com.codicesoftware.plugins.hudson.model;
+
+import com.codicesoftware.plugins.jenkins.mergebot.ObjectSpecType;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ObjectSpecTest {
+
+    @Test
+    public void testConstructor() {
+        ObjectSpec underTest = new ObjectSpec(ObjectSpecType.Changeset, 111, "testing", "domain.com:8084");
+        assertEquals(ObjectSpecType.Changeset, underTest.getType());
+        assertEquals(111, underTest.getId());
+        assertEquals("testing", underTest.getRepository());
+        assertEquals("domain.com:8084", underTest.getServer());
+        assertEquals("cs:111@testing@domain.com:8084", underTest.getFullSpec());
+    }
+
+    @Test
+    public void testConstructorSsl() {
+        ObjectSpec underTest = new ObjectSpec(ObjectSpecType.Shelve, 111, "testing", "ssl://domain.com:8088");
+        assertEquals(ObjectSpecType.Shelve, underTest.getType());
+        assertEquals(111, underTest.getId());
+        assertEquals("testing", underTest.getRepository());
+        assertEquals("ssl://domain.com:8088", underTest.getServer());
+        assertEquals("sh:111@testing@ssl://domain.com:8088", underTest.getFullSpec());
+    }
+
+    @Test
+    public void testConstructorCloud() {
+        ObjectSpec underTest = new ObjectSpec(ObjectSpecType.Changeset, 111, "testing", "myOrganization@cloud");
+        assertEquals(ObjectSpecType.Changeset, underTest.getType());
+        assertEquals(111, underTest.getId());
+        assertEquals("testing", underTest.getRepository());
+        assertEquals("myOrganization@cloud", underTest.getServer());
+        assertEquals("cs:111@testing@myOrganization@cloud", underTest.getFullSpec());
+    }
+}

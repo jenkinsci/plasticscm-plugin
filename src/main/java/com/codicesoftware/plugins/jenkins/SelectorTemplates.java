bc464b3928a4bfc7f277321ae6f7f5f0b7dac5d4
Miguel González <miguel.gonzalez@unity3d.com>
2023-07-19 15:59:35 +0200

    Fix default selector
    Signed-off-by: Miguel González <miguel.gonzalez@unity3d.com>

--- src/main/java/com/codicesoftware/plugins/jenkins/SelectorTemplates.java
+++ src/main/java/com/codicesoftware/plugins/jenkins/SelectorTemplates.java
@@ -5,7 +5,7 @@ public class SelectorTemplates {
     private SelectorTemplates() {
     }
 
-    public static final String DEFAULT = "repository \"default\"%n  path \"/\"%n    smartbranch \"main\"";
+    public static final String DEFAULT = "repository \"default\"\n  path \"/\"\n    smartbranch \"main\"";
     public static final String BRANCH = "repository \"%s@%s\"%n  path \"/\"%n    smartbranch \"%s\"";
     public static final String CHANGESET =
             "repository \"%s@%s\"%n  path \"/\"%n    smartbranch \"%s\" changeset \"%s\"";
